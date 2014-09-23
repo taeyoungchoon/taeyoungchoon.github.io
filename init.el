162ffde2d32038db1f662b85affcdd95c26ea2e0
taeyoungchoon <taeyoungchoon@gmail.com>
2014-09-23 23:26:58 +0900

    fix init.el

--- init.el
+++ init.el
@@ -1,14 +1,34 @@
+; package
 (require 'package)
 (add-to-list 'package-archives
   '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
 
+; init
+(setq inhibit-splash-screen t)
+(visual-line-mode)
+(display-time)
+
+; init > variables
 (custom-set-variables
+ '(custom-enabled-themes (quote (wombat)))
  '(default-input-method "korean-hangul3f")
+ '(make-backup-files nil)
  )
 
+; encoding
 (prefer-coding-system 'utf-8)
 (set-file-name-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
 (set-buffer-file-coding-system 'utf-8)
+
+; move around
+(global-set-key (kbd "C-x <up>") 'windmove-up)
+(global-set-key (kbd "C-x <down>") 'windmove-down)
+(global-set-key (kbd "C-x <right>") 'windmove-right)
+(global-set-key (kbd "C-x <left>") 'windmove-left)
+
+(require 'ace-jump-mode)
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
