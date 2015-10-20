eb710c5f139a86825fcc4b8e8953e687ebb610a6
taeyoungchoon <taeyoungchoon@gmail.com>
2015-10-20 23:44:21 +0900

    done

--- foo.lisp
+++ foo.lisp
@@ -1,1 +1,16 @@
 (princ "hello, hack!")
+
+(+ 1 2 3)
+
+(cons t nil)
+(list 1 2 3)
+
+(setf lst (list 1 2 3))
+
+(if t t)
+(when t t)
+
+
+
+
+
--- foo.pl
+++ foo.pl
@@ -1,3 +1,13 @@
 #!/usr/bin/perl -w
 
 print "hello, hack!\n";
+
+print $timestamp = `date +%Y%m%d.%H%M%S`;
+
+__END__
+
+foreach $item (<>) {
+    print $item;
+}
+
+split and join
