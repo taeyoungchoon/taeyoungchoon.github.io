3a60f8cf5dfc9891c1d4dfd9f7f7a343685d7ac1
taeyoungchoon <taeyoungchoon@gmail.com>
2015-11-03 22:11:08 +0900

    lots of

--- lotto.lisp
+++ lotto.lisp
@@ -6,14 +6,23 @@
 ;; sort
 ;;
 
+#|
 (defun t-random (max)
   (+ (random (- max 1)) 1))
 
-#|
 (defun t-random-test (max)
   (loop for i from 1 to 5
     do (print (t-random (max)))))
-|#
 
 (let ((lt (loop for num from 1 to 60 collect num)))
   (nth 10 lt))
+|#
+
+(setf lt nil)
+
+;;loop unil big enough
+(setf pick (+ (random 60) 1))
+(member pick lt)
+(push pick lt)
+(length lt)
+;;if lt is big enough, out
