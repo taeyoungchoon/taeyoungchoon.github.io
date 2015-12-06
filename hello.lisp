cff48c6689bbafa89e425dcdfed5cb15865ad654
taeyoungchoon <taeyoungchoon@gmail.com>
2015-12-06 22:31:17 +0900

    done

--- hello.lisp
+++ hello.lisp
@@ -1,4 +1,17 @@
-(defun hello-world () 
-  (format t "helo, world"))
+;;;
+;;; hello-world as always
+;;;
 
-(hello-world)
+(defparameter type-of-nubmers '((one two tree)
+				(uno duo tre)
+				(1 2 3)))
+
+(defun say-number ()
+  (let ((english '(one two tree))
+	(italian '(uno duo tre)))
+    (format t "~% ~a" english)
+    (format t "~% ~a" italian)))
+
+(loop for i in type-of-nubmers
+   do (princ i))
+
