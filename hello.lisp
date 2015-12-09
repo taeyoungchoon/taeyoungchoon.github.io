953bf2412a5cd54b07561926618f16fa2d546cac
taeyoungchoon <taeyoungchoon@gmail.com>
2015-12-09 17:38:09 +0900

    done

--- hello.lisp
+++ hello.lisp
@@ -1,11 +1,11 @@
 ;;; hello.lisp
 
 (defun hello-func ()
+  "this is the hello function"
   (format t "~% hello"))
 
 (defmacro hello-macro()
   `(let ((hello "hello, world"))
      (format t "~% ~a" hello)))
 
-;;(hello-macro)
-(hello-func)
+(hello-macro)
