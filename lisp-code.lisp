210cb9576169a18afed1432e0f27f7fd65ba4607
taeyoungchoon <taeyoungchoon@gmail.com>
2015-12-10 13:14:27 +0900

    done

--- lisp-code.lisp
+++ lisp-code.lisp
@@ -16,21 +16,17 @@
 (format nil "hi")
 (if t 1 2)
 (when t 1)
+(if t (progn 1 2 3))
+(if t (prog 1 2 3))
 (cond (1 2)(3 4))
+(cond (nil 1)((> 2 3) 4)(t 5))
 (loop for i from 1 to 3 collect i)
 (defun hello ()
   (princ "hi"))
 (defmacro jack (body)
   `(princ ,body))
 ;;(jack "hi")
-(case 2
-  (1 'a)
-  (2 'b))
-(if t
-    (progn
-      'a
-      'b)
-    'c)
+(case 2 (1 'a)(2 'b))
 (and 1 1)
 (or 1 1)
 (not 1)
