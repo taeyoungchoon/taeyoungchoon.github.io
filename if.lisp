89db73299d3135939dadc88e72b6cd93e1857f10
taeyoungchoon <taeyoungchoon@gmail.com>
2015-12-15 23:28:07 +0900

    done

--- if.lisp
+++ if.lisp
@@ -7,3 +7,24 @@
   (if (> *choose* 1)
       'big-dream
       'cute))
+
+(defun alive-p () )
+(defun brave-p () )
+
+(if 'alive-p
+    'brave-p)
+
+(if (and 'alive-p 'brave-p)
+    'brave)
+
+(if t
+    (if t 'tt))
+
+(let ((x 1) (y 2))
+  (if (> x y)
+      'x
+      'y))
+
+(if t
+    (let ((x 1) (y 2))
+      (format t "~a~%" (+ x y))))
