b50d843961b4771b9166c37c659c1179bb8db0c7
taeyoungchoon <taeyoungchoon@gmail.com>
2017-03-28 11:22:32 +0900

    done

---
+++ oracle_alert_chk.bat
@@ -0,0 +1,34 @@
+* ftp
+
+set FTP_SVR=192.168.0.101
+set FTP_DEST_DIR=/remote_oracle_log
+set FTP_DEP_DIR=d:\oracle_alert_check
+
+cd D:\oracle_alert_check
+
+find "ORA-" < X:\app\Administrator\diag\rdbms\pantechora\pantecho\trace\alert_pantecho.log > D:\oracle_alert_check\temp.txt
+
+for %%F in ("D:\oracle_alert_check\temp.txt") do if %%~zF equ 0 del "%%F"
+
+if exist D:\oracle_alert_check\temp.txt (type D:\oracle_alert_check\title.txt D:\oracle_alert_check\temp.txt D:\oracle_alert_check\bottom.txt > D:\oracle_alert_check\alert_pantecho_17.log) else (echo file was not found)
+
+@echo off
+echo open %FTP_SVR% > d:\oracle_alert_check\ftpcmd.cmd
+echo user >> d:\oracle_alert_check\ftpcmd.cmd
+echo ocm>> d:\oracle_alert_check\ftpcmd.cmd
+echo ocm>> d:\oracle_alert_check\ftpcmd.cmd
+echo cd %FTP_DEST_DIR% >> d:\oracle_alert_check\ftpcmd.cmd
+echo lcd %FTP_DEP_DIR% >> d:\oracle_alert_check\ftpcmd.cmd
+echo bin >> d:\oracle_alert_check\ftpcmd.cmd
+echo has >> d:\oracle_alert_check\ftpcmd.cmd
+echo prompt >> d:\oracle_alert_check\ftpcmd.cmd
+echo mput *.log >> d:\oracle_alert_check\ftpcmd.cmd
+echo quit >> d:\oracle_alert_check\ftpcmd.cmd
+
+ftp -n -s:d:\oracle_alert_check\ftpcmd.cmd
+
+del d:\oracle_alert_check\*.log
+del d:\oracle_alert_check\temp.txt
+del d:\oracle_alert_check\ftpcmd.cmd
+
+exit
