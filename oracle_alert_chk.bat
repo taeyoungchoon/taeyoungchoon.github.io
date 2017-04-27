3bf0f85a8b785449a19771a7fa9ff7509d977957
taeyoungchoon <taeyoungchoon@gmail.com>
2017-04-27 09:30:49 +0900

    done

--- oracle_alert_chk.bat
+++ oracle_alert_chk.bat
@@ -1,4 +1,4 @@
-* ftp
+%* ftp
 
 set FTP_SVR=192.168.0.101
 set FTP_DEST_DIR=/remote_oracle_log
@@ -6,11 +6,11 @@ set FTP_DEP_DIR=d:\oracle_alert_check
 
 cd D:\oracle_alert_check
 
-find "ORA-" < X:\app\Administrator\diag\rdbms\pantechora\pantecho\trace\alert_pantecho.log > D:\oracle_alert_check\temp.txt
+find "ORA-" < X:\app\Administrator\diag\rdbms\raulora\raulo\trace\alert_raulo.log > D:\oracle_alert_check\temp.txt
 
 for %%F in ("D:\oracle_alert_check\temp.txt") do if %%~zF equ 0 del "%%F"
 
-if exist D:\oracle_alert_check\temp.txt (type D:\oracle_alert_check\title.txt D:\oracle_alert_check\temp.txt D:\oracle_alert_check\bottom.txt > D:\oracle_alert_check\alert_pantecho_17.log) else (echo file was not found)
+if exist D:\oracle_alert_check\temp.txt (type D:\oracle_alert_check\title.txt D:\oracle_alert_check\temp.txt D:\oracle_alert_check\bottom.txt > D:\oracle_alert_check\alert_raulo_17.log) else (echo file was not found)
 
 @echo off
 echo open %FTP_SVR% > d:\oracle_alert_check\ftpcmd.cmd
