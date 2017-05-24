696ea454fb101c403323aee45db8e632deb2c7f6
taeyoungchoon <taeyoungchoon@gmail.com>
2017-05-24 16:53:11 +0900

    done

--- linux-ma.sh
+++ linux-ma.sh
@@ -1,15 +1,27 @@
+export PS1="# "
+clear
 date
-uname -a
-lsb_release -a
-uptime
+dmcdecode --type 1 | grep Product
+cat /etc/redhat-release
+hostname
+dmcdecode --type 4 | grep Version | uniq
+dmcdecode --type 4 | grep Version | wc -l
 
-df -Pk
-df -h
-pstree
-lvs
+top -n 1 | grep ^Cpu
+free -m | grep buffers/ | awk '{ print $3*100/($3+$4) }'
+free -m | grep Mem
+free -m | grep Swap | awk '{ print $3*100/($3+$4) }'
+
+df -h | head -1
+df -h | grep /$
+df -h | sort -k5
 
 netstat -rn
-ethtool
-ifconfig
-netstat -an
+netstat --inet -an
+ifconfig bond0
 
+ps -ef | wc -l
+uptime
+w
+cat /var/log/messages | egrep "warn|erro|crit" | wc -l
+cat /var/log/messages | egrep "warn|erro|crit" | tail -5
