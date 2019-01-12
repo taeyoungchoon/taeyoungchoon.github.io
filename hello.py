9d8ab8fa7b6cebd529de84398fe686cb295b01b7
taeyoungchoon <taeyoungchoon@gmail.com>
2019-01-13 00:28:52 +0900

    d

--- hello.py
+++ hello.py
@@ -1,5 +1,30 @@
+#!/usr/bin/env python
+
+# from os import *
+
+lt1 = [0, 1, 2]
+lt2 = ['apple', 'coffee', 'love']
+kv1 = {1:2, 3:4}
+kv2 = {'editor':'emacs', 'note':'moleskine'}
+
 def hello( name ):
     print "hello", name
     return
 
 hello( name="world")
+
+print "I am using %s and %s" % (kv2['editor'], kv2['note'])
+
+if True:
+    print """
+this is the poem
+or the end of it
+or not"""
+
+# print "\r"
+    
+for number in range(5):
+    print number,
+
+# in = raw_input('>')
+# print "Your input: %s" % in
