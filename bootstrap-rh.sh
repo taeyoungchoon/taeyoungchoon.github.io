b9cfe0b76021e7a7cf39ba23ed5eb8293f4da916
taeyoungchoon <taeyoungchoon@gmail.com>
2019-07-23 10:38:59 +0900

    d

--- bootstrap-rh.sh
+++ bootstrap-rh.sh
@@ -1,5 +1,5 @@
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/sshd/sshd_config
 systemctl restart sshd
 
-systemctl enable firewalld
-systemctl start firewalld
+yum install git -y
+
