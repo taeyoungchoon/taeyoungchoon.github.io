681a4d7f30d6d89433023aa87b49e2c02441e6e8
jack <jack@light.foo.com>
2022-02-19 20:47:35 +0900

    d

--- init.el
+++ init.el
@@ -1,8 +1,9 @@
 (require 'package)
 (add-to-list 'package-archives
-  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+(setq ring-bell-function 'ignore)
 (global-visual-line-mode t)
 (setq inhibit-splash-screen t)
 (display-time)
@@ -113,12 +114,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(display-time-mode t)
  '(elfeed-feeds
-   (quote
-    ("https://lwn.net/headlines/rss" "http://www.50ply.com/atom.xml" "http://nullprogram.com/feed/" "http://www.terminally-incoherent.com/blog/feed/"))))
+   '("https://lwn.net/headlines/rss" "http://www.50ply.com/atom.xml" "http://nullprogram.com/feed/" "http://www.terminally-incoherent.com/blog/feed/")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "D2Coding ligature" :foundry "RIXF" :slant normal :weight normal :height 158 :width normal)))))
