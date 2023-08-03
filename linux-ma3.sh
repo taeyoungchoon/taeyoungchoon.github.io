e3b47338bd7f3adcaa4318bfd201f931719d426e
taeyoungchoon <taeyoungchoon@gmail.com>
2023-08-03 08:01:26 +0100

    d

--- linux-ma3.sh
+++ linux-ma3.sh
@@ -1,7 +1,7 @@
 # p0: timestamp
 date
 # p1: os
-cat /etc/redhat-release
+test -e /etc/redhat-release && cat /etc/redhat-release || cat /etc/os-release | egrep -i "^(name|version=)"
 # p2: product
 dmidecode --type 1 | egrep -i product
 # p3: hostname
@@ -9,26 +9,28 @@ hostname
 # p4: cpu
 cat /proc/cpuinfo | grep 'model n' | uniq
 # p5: memory 
-free -m
+free -m | grep Mem
 # p6: kernel version
-uname -r
+uname -a
 # p7: cpu usage 
-top -b -n 1 | grep Cpu
+top -b -n 1 | grep Cpu | awk '{ print $2 + $4 }'
 # p8: memory usage
-if free | grep avail &>/dev/null; then 
+if free | grep avail &>/dev/null; then
+    echo avail
     free -m | awk '/Mem/ { print ($2-$7)*100/$2}'
 else
+    echo old
     free -m | awk '/cache:/ { print $3*100/($3+$4) }'
 fi
 # p9: disk usage
-df -Ph | column -t | sed 's/%//' | sort -k5 -r -n
+df -Ph | column -t | sed 's/%//' | sort -k5 -r -n | head -5
 # p10: system log
 #-journalctl -b --no-pager -p crit(2), err(3), warning(4) -p 2..4
-if which journalctl &>/dev/null; then
-    journalctl -b --no-pager -p 2..4 | tail -3 | cat -n
-else
-    cat /var/log/messages | egrep -i "warn|erro|crit" | tail -3 | cat -n
-fi
+# if which journalctl &>/dev/null; then
+#     journalctl -b --no-pager -p 2..4 | tail -3 | cat -n
+# else
+#     cat /var/log/messages | egrep -i "warn|erro|crit" | tail -3 | cat -n
+# fi
 # p11: boot log
 dmesg | tail -3 | cat -n
 # p12: last logged in users
@@ -40,7 +42,5 @@ last | head -3 | cat -n
 ip r | awk '/^default/ { system("ip a s " $5) }' | awk '/inet / { print $2 }'
 ip r | awk '/^default/ { system("ping -c 3 " $3) }'
 # fine ----------------------------------------------------------------------
-# uname -a
-# 
-#
-
+dmidecode --type memory | egrep "Size:.*(MB|GB)"
+which journalctll
