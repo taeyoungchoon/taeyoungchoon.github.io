9c9602042255e42286bc8c7578ef01edf7c3f3be
taeyoungchoon <taeyoungchoon@gmail.com>
2023-08-08 04:59:24 +0100

    d

--- linux-ma4.sh
+++ linux-ma4.sh
@@ -2,16 +2,17 @@
 date
 # p1: os
 test -e /etc/redhat-release && cat /etc/redhat-release || cat /etc/os-release | egrep -i "^(name|version=)"
+uname -m
 # p2: product
-dmidecode --type 1 | egrep -i product
+dmidecode --type 1 | grep -i product
 # p3: hostname
 hostname
 # p4: cpu
 cat /proc/cpuinfo | grep 'model n' | uniq
 # p5: memory 
-free -m | grep Mem
+free -g | grep Mem
 # p6: kernel version
-uname -a
+uname -r
 # p7: cpu usage 
 top -b -n 1 | grep Cpu | awk '{ print $2 + $4 }'
 # p8: memory usage
@@ -23,14 +24,15 @@ else
     free -m | awk '/cache:/ { print $3*100/($3+$4) }'
 fi
 # p9: disk usage
-df -Ph | column -t | sed 's/%//' | sort -k5 -r -n | head -5
+df -Ph -x tmpfs -x devtmpfs | column -t | sed 's/%//' | sort -k5 -r -n | head -5
 # p10: system log
 #-journalctl -b --no-pager -p crit(2), err(3), warning(4) -p 2..4
-# if which journalctl &>/dev/null; then
+if which journalctl &>/dev/null; then
+  journalctl -n 3 | cat -n
 #     journalctl -b --no-pager -p 2..4 | tail -3 | cat -n
-# else
-#     cat /var/log/messages | egrep -i "warn|erro|crit" | tail -3 | cat -n
-# fi
+else
+    cat /var/log/messages | egrep -i "warn|erro|crit" | tail -3 | cat -n
+fi
 # p11: boot log
 dmesg | tail -3 | cat -n
 # p12: last logged in users
@@ -39,8 +41,13 @@ last | head -3 | cat -n
 #history -c
 #export HISTSIZE=0
 # p14: network
-ip r | awk '/^default/ { system("ip a s " $5) }' | awk '/inet / { print $2 }'
+echo my ip $(ip r | awk '/^default/ { system("ip a s " $5) }' | awk '/inet / { print $2 }')
 ip r | awk '/^default/ { system("ping -c 3 " $3) }'
 # fine ----------------------------------------------------------------------
-dmidecode --type memory | egrep "Size:.*(MB|GB)" | uniq -c
-which journalctll
+#dmidecode --type memory | egrep "Size:.*(MB|GB)" | uniq -c
+#echo '16 * 4' | bc
+#
+#dmidecode --type processor | grep Version | wc -l
+#cat /proc/cpuinfo  | grep 'model n' | wc -l
+#grep ht /proc/cpuinfo >/dev/null; echo $?
+history | tail -3
